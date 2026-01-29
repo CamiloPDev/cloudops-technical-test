a8b3485fbccba1a401d839beee7f37af280f895a
CamiloDev <cami20032017@gmail.com>
2026-01-29 17:44:46 -0500

    feat: add arm in ecs

--- iac/modules/ecs/main.tf
+++ iac/modules/ecs/main.tf
@@ -148,6 +148,11 @@ resource "aws_ecs_task_definition" "services" {
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
   task_role_arn            = aws_iam_role.ecs_task.arn
 
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+
   container_definitions = jsonencode([{
     name      = each.value.name
     image     = each.value.image
